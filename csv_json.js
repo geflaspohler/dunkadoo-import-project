5cecb4ba5ebd4fc33ae8a64cb1e6e723c7700f7e
Geneveive Flaspohler <genevee@umich.edu>
2016-06-07 14:29:18 -0400

    csv_Json posts

--- csv_json.js
+++ csv_json.js
@@ -1,91 +1,195 @@
-node_cj = require("node-csv-json");
-var prompt = require('prompt');
-var RSVP = require('rsvp');
-var fs = require('fs');
-var async = require('async');
+//Includes 
+node_cj = require("node-csv-json");//CSV to JSON 
+var prompt = require('prompt');//For user input
+var RSVP = require('rsvp');//For promises
+var fs = require('fs');//For reading from filesystem
+var async = require('async');//For async requests
+//var $ = require('jQuery');
+var $ = jQuery = require('jquery'); // In Node.js
+var http = require('http');
+var request = require('request');
 
+var string_data = '[{"local_id":3,"uuid":"b7e43d56-cc32-4f33-8bf0-e7c91adb76d4","projectId":4,"speciesId":76,"count":2,"user":"gflaspohler@dunkadoo.org","timestamp":"2016-06-07T13:46:03-04:00","latitude":42.2830168,"longitude":-83.7485074,"accuracy":18.85300064086914,"gps_at":"2016-06-07T13:46:04-04:00","data":"{\"age\":\"\",\"sex\":\"\",\"band\":\"\",\"rare\":\"\",\"size\":\"\",\"behavior\":[],\"distance\":\"\",\"direction\":\"\",\"observation\":[]}","device":"e17092ac5955022e","last_updated":"2016-06-07T13:46:04-04:00","ready_submit":"2016-06-07T13:46:04-04:00"}]';
+
+var json_bird_data =  
+{
+	"local_id":3,
+	"uuid":"4f4f8fee-e461-43be-9746-3dad0455d5a8",
+	"projectId":4,
+	"speciesId":77,
+	"count":5,
+	"user":"gflaspohler@dunkadoo.org",
+	"timestamp":"2016-06-07T13:46:03-04:00",
+	"latitude":42.2830168,
+	"longitude":-83.7485074,
+	"accuracy":18.85300064086914,
+	"gps_at":"2016-06-07T13:46:04-04:00",
+	"data":"{\"age\":\"\",\"sex\":\"\",\"band\":\"\",\"rare\":\"\",\"size\":\"\",\"behavior\":[],\"distance\":\"\",\"direction\":\"\",\"observation\":[]}",
+	"device":"e17092ac5955022e",
+	"last_updated":"2016-06-07T13:46:04-04:00",
+	"ready_submit":"2016-06-07T13:46:04-04:00"
+}
+
+/*var json_data =  
+{
+	SpeciesData: { 
+		age: "I - Immature", 
+		morph: "Light"
+	}, 
+};*/
+
+
+/*var json_data =  
+{
+	"uuid": "3f4f8fee-e461-43be-9746-3dad0455d5a8" , 
+	"projectid": "temp", 
+	"device": "temp"
+};*/
+//Change user input prompt defaults
 prompt.message = '';
 prompt.start();
 
-var userColumns = {};
-var species= {};
-var studySpecific = [];
-var speciesSpecific = [];
+var userColumns = {};//Stores user specific names for data columns
+var species= {};//Stores study specific taxonomy data
+var studySpecific = [];//Stores user specific names for study specific columns
+var speciesSpecific = [];//Stores user sepcific names for species specific columns
 
+//Reads in a CSV containing taxonomy, assumeing species code and species 
+//common name are in seperate columns with parenthese enclosed junk
+//ex. BE, Bald Eagle (JUNK); final taxonomy stored in the species hash should
+//be in the form "BE - Bald Eagle". Species are also assigned random IDs
 var readTaxonomy = new Promise(function(resolve, reject){
-	console.log('Reading taxonomy!');
 	var x = 0;
-	var fileContents = fs.readFileSync("HWI.csv");
-	var lines = fileContents.toString().split('\r');
-	for (var i = 0; i < lines.length; i++) {
-		var entry = lines[i].toString().split(',');
+	var fileContents = fs.readFileSync("HWI.csv");//Read the taxonomy csv
+	var lines = fileContents.toString().split('\r');//First split into rows
+	for (var i = 0; i < lines.length; i++) {//For each row
+		var entry = lines[i].toString().split(',');//Split into entires
 		var common_name = entry[1].substring(0, entry[1].indexOf('(')-1);//-1 to account for space
-		species[(entry[0] + " - " + common_name)] = ++x;
+		species[(entry[0] + " - " + common_name)] = ++x;//Assign a speciesID
 	}
-	resolve(species);
+	//species.forEach(function(currentValue, index, array){
+	resolve(species);//Fufill the promise
 });
 
+//Read in the CSV of the user observations in JSON format
 var readCSV = new Promise(function(resolve, reject){
-	console.log('Reading CSV!');
 	node_cj({
-		input: "Grand Canyon 2015 v_ 2 - Default Questions.csv",
-		output: "output.json"
+		//input: "Grand Canyon 2015 v_ 2 - Default Questions.csv",
+		//input: 'Grand Canyon 2015 v_ 2 - AK.csv',  
+		input: "Grand Canyon 2015 v_ 2 - BW_ RT_ FH.csv",
+		output: "output.json" //Output is unused
 	}, function(err, result){
-		if(err){
+		if(err){//If error occurs, throw
 			console.log(err); throw err;	
 			reject(err);
 		}
 		else{
-			resolve(result);
-		}
-	});
-});
+			resolve(result);//Resolve the promise and return the JSON result
+		}//End else
+	});//End node_cj
+});//End promise
 
+//Builds and prints the JSON packets from taxnomy and observation data
 function printJSON(data){
-	console.log("Starting to print JSON");
-	console.log(userColumns);
-	var observations = data[0];
-	var taxonomies = data[1];
-	var json_data = [];
-	observations.forEach(function(currentValue, index, array){
-		var json_packet = '';
-		var uuid = generateUUID();
-		//For this to work, curentValue must be a JSON entry with a hash-mapped
-		//"Year", "Month", "Day", "Hour", "Minute", "Second", "Time Zone" fields
-		var timestamp = getTimestamp(currentValue);
+	return new Promise(function(resolve, reject){
+		//"data" is an array containing the [observations taxonomy] data from
+		//previous promises. We create refernces for clarity
+		var observations = data[0];
+		var taxonomies = data[1];
 
-		json_packet +="{ " + '\n';
-		json_packet += " 'uuid': " + uuid + ', \n';
-		json_packet += " 'projectid': " + "temp" + ', \n';
-		json_packet += " 'speciesid': " + taxonomies[currentValue[userColumns['Observed']]] + ', \n';
-		json_packet += " 'count': " + currentValue[userColumns['Count']] + ', \n';
-		json_packet += " 'user': " + currentValue[userColumns["Technician"]] + ', \n';
-		json_packet += " 'timestamp': " + timestamp + ', \n';
-		json_packet += " 'latitude': " + currentValue[userColumns["Longitude"]] + ', \n';
-		json_packet += " 'longitude': " + currentValue[userColumns["Latitude"]] + ', \n';
-		json_packet += " 'gps_at': " + timestamp + ', \n';
-		if(studySpecific[0] !== ''){
-			json_packet += " 'data:' { " + '\n';
-			for(var i = 0; i < studySpecific.length; i++){
-				json_packet += '\t' + "'" + studySpecific[i] + "': " + currentValue[studySpecific[i]] + ', \n';
+		var json_data = [];//Stores the array of final JSON packets
+		
+		var total_processed = 0;
+		//For each entry in observations
+		observations.forEach(function(currentValue, index, array){
+			var json_packet = {};
+			var uuid = generateUUID();
+			//For this to work, curentValue must be a JSON entry with a hash-mapped
+			//"Year", "Month", "Day", "Hour", "Minute", "Second", "Time Zone" fields
+			var timestamp = getTimestamp(currentValue);
+			
+			json_packet +="{ " + '\n';
+			json_packet += ' "uuid": ' + '"' + uuid + '" , \n';
+			json_packet += ' "projectid": "' + "temp" + '", \n';
+			json_packet += ' "speciesid": ' + taxonomies[currentValue[userColumns['Observed']]] + ', \n';
+			json_packet += ' "count": ' + currentValue[userColumns['Count']] + ', \n';
+			json_packet += ' "user": "' + currentValue[userColumns["Technician"]] + '", \n';
+			json_packet += ' "timestamp": "' + timestamp + '", \n';
+			json_packet += ' "latitude": ' + currentValue[userColumns["Latitude"]] + ', \n';
+			json_packet += ' "longitude": ' + currentValue[userColumns["Longitude"]] + ', \n';
+			json_packet += ' "gps_at": "' + timestamp + '", \n';
+			//Only print study specific information if user has provided 
+			if(studySpecific && studySpecific[0] !== '' && studySpecific[0] !== ' '){
+				var data_printed = false;
+				for(var i = 0; i < studySpecific.length; i++){
+					if(currentValue[studySpecific[i]] && currentValue[studySpecific[i]] !== '' && currentValue[studySpecific[i]] !== " "){
+						if(data_printed === false){
+							json_packet += ' "data": { ' + '\n';
+							data_printed = true;
+						}
+						json_packet += '\t' + '"' + studySpecific[i] + '": "' + currentValue[studySpecific[i]] + '", \n';
+					}
+				}
+				if(data_printed === true){
+					json_packet = json_packet.substring(0, json_packet.length - 3);
+					json_packet += "\n  }" + ', \n';
+				}
 			}
-			json_packet += "  }" + ', \n';
-		}
-		if(speciesSpecific[0] !== ''){
-			json_packet += " 'SpeciesData:' { " + '\n';
-			for(var i = 0; i < studySpecific.length; i++){
-				json_packet += '\t' + "'" + speciesSpecific[i] + "': " + currentValue[speciesSpecific[i]] + ', \n';
+			//Only print species specific information if user has provided 
+			if(speciesSpecific && speciesSpecific[0] !== '' && speciesSpecific[0] !== ' '){
+				var species_data_printed = false;
+				for(var i = 0; i < speciesSpecific.length; i++){
+					var cname = speciesSpecific[i];
+					var cdata = currentValue[speciesSpecific[i]];
+					if(cdata && cdata !== '' && cdata !== " "){
+						if(species_data_printed === false){
+							json_packet += ' "SpeciesData:" { ' + '\n';
+							species_data_printed = true;
+						}
+
+						if(cname.toLowerCase().includes('age')){
+							json_packet += '\t' + '"' + "age" + '": "' + currentValue[speciesSpecific[i]] + '", \n';
+						}//End inner if
+						else if(cname.toLowerCase().includes('gender')){
+							json_packet += '\t' + '"' + 'sex' + '": "' + currentValue[speciesSpecific[i]] + '", \n';
+						}//End inner if
+						else if(cname.toLowerCase().includes('age')){
+							json_packet += '\t' + '"' + 'age' + '": "' + currentValue[speciesSpecific[i]] + '", \n';
+						}//End inner if
+						else if(cname.toLowerCase().includes('key')){
+							json_packet += '\t' + '"' + 'key' + '": "' + currentValue[speciesSpecific[i]] + '", \n';
+						}//End inner if
+						else if(cname.toLowerCase().includes('morph')){
+							json_packet += '\t' + '"' + 'morph' + '": "' + currentValue[speciesSpecific[i]] + '", \n';
+						}//End inner if
+						else if(cname.toLowerCase().includes('molt')){
+							json_packet += '\t' + '"' + 'molt' + '": "' + currentValue[speciesSpecific[i]] + '", \n';
+						}//End inner if
+						else{
+							json_packet += '\t' + '"' + 'raw attribute' + '": ' + currentValue[speciesSpecific[i]] + ', \n';
+						}	
+					}//End outer if
+				}
+				if(species_data_printed === true){
+					json_packet = json_packet.substring(0, json_packet.length - 3);
+					json_packet += "\n  }" + ', \n';
+				}
 			}
-			json_packet += "  }" + ', \n';
-		}
-		json_packet += " 'device': " + "temp" + '\n';
-		json_packet +=" }";
-		console.log(currentValue);
-		console.log(json_packet);
-		json_data.push(json_packet);
+			json_packet += ' "device": ' + '"temp"' + '\n';
+			json_packet +=" }";
+			console.log(json_packet);
+			json_data.push(json_packet);
+			total_processed++;
+			console.log("Proccessed " + total_processed + " out of " + observations.length);
+			if(total_processed === observations.length){
+				resolve(json_data);
+			}
+		});
 	});
+	//console.log("Species: "  + speciesSpecific + ", " + speciesSpecific[0].toLowerCase().includes('gender'));
 }
 
+//Generate a random unique id for each observation
 var generateUUID = function(){
 	var d = new Date().getTime();
 	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -96,12 +200,15 @@ var generateUUID = function(){
 	return uuid;
 }
 
+//Get an ISO 8601 timestamp from an obejct that has date information
 var getTimestamp = function(t){
 	return t['Year'] + '-' + t['Month'] + '-' +  t['Day']
 		+ 'T' + ((t['Hour'].length === 2) ? t['Hour'] : ('0'+t['Hour'])) 
 		+ ':' + t['Minute'] + ':' + t['Second'] + t['Time Zone'] + '.00'; 
 }
 
+//Get user input for study and species specific column names. Should be 
+//entered as a comma seperated list. eg. name,age,gender
 var getUserTaxonomy= function(callback){
 	console.log("Enter the names of your study specific columns as a comma seperated list:");
 	prompt.get(['Columns'], function (err, result) {
@@ -117,6 +224,7 @@ var getUserTaxonomy= function(callback){
 		});
 	});
 }
+//Get user input for custom column names; should be entered as prompted
 var getUserColumn= function(callback){
 	console.log("Getting user input for column!");
 	prompt.get(['Species Count'], function (err, result) {
@@ -141,17 +249,116 @@ var getUserColumn= function(callback){
 		});
 	});
 }
+//Error fucntion
+function onErr(err) {console.log(err);throw err;}
 
+var sendObservations = function (observationJSON, submission_reflects_time){
 
+	var uploadAddress = 'https://app-dev.dunkadoo.org/api/v1/observations/?apikey=003ae3b9c7b1b00b2e85888ab6f3501d';
+	request({
+		url: uploadAddress,
+		method: "POST",
+		json: json_bird_data 
+	}, function (error, response, body) {
+        if (!error && (response.statusCode === 200 || response.statusCode === 201)) {
+            console.log(body)
+        }
+        else {
+            console.log("error: " + error)
+            console.log("response.statusCode: " + response.statusCode)
+            console.log("response.statusText: " + response.statusText)
+        }
+    })
+}
+ 
+/*var sendObservations = function (observationJSON, submission_reflects_time){
+	console.log("Enteriting send obs");	
+	//console.log('POST:[' + JSON.stringify(json_data) + ']'); 
+
+	//var uploadAddress = $rootScope.apiProtocol + $rootScope.apibase + '/api/v1/observations?apikey=' + $rootScope.apikey;
+
+	var uploadAddress = 'https://app-dev.dunkadoo.org/api/v1/observations/?apikey=003ae3b9c7b1b00b2e85888ab6f3501d';
+	var options = {
+		method: "POST",
+		url: uploadAddress,
+		success: function (data) { console.log(data);
+		},
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		data: string_data,
+		dataType: 'json'
+	};
+
+	function callback(error, response, body){
+		if(!error){
+			console.log("error: " + error);
+			console.log(response);
+			console.log("body: " + body);
+			console.log('Request without error!');
+			//observationSubmitted(APIResponse, APIStatus, APIXHR, submission_reflects_time);
+			//observationSubmitted(response, error, submission_reflects_time);
+		}
+		else{ console.log('Error: ' + error);}
+	}
+
+	request(options, callback);
+}*/
+
+//Main functionatily 
 async.series([
 	getUserTaxonomy, 
-	getUserColumn
+	getUserColumn, 
 ], function(error, results){
 	if(error) throw error;
+	console.log("Got to promise all");
 	Promise.all([readCSV, readTaxonomy]).then(function(result){
-		printJSON(result);
-		console.log("printed json");
+		console.log("Printing JSON");
+		printJSON(result).then(function(observationPayload){
+			try{sendObservations(observationPayload, "2016-06-07T13:46:04-04:00");}
+			catch(err){console.log(err)}
+		}, function(err){console.log(err);});
 	}, function(err){console.log(err)});
 });
 
-function onErr(err) {console.log(err);throw err;}
+var observationSubmitted = function (APIResponse, APIStatus, APIXHR, submission_reflects_time) {
+        console.log('API Response'); 
+         console.log(APIResponse); 
+         console.log(APIStatus); 
+         console.log(APIXHR); 
+         console.log(submission_reflects_time); /*
+
+        var curTimeTempt = moment();
+
+        var submissionResults = [curTimeTempt.format(), APIXHR.status, submission_reflects_time];
+
+        $rootScope.db.executeSql('INSERT INTO submissions ("timestamp","result","last_observation_at") VALUES (?,?,?)', submissionResults, function (res) {
+            if (submitDebug) { console.log(submissionResults) };
+        }, function (error) {
+            console.log('Unable to Update Submissions Table');
+        });
+
+        var APIObservationInfo = [];
+
+        for (var this_obs = 0; this_obs < APIResponse.length; this_obs++) {
+            
+            var this_obs_uuid = Object.keys(APIResponse[this_obs])[0];
+            var this_obs_id = APIResponse[this_obs][this_obs_uuid]['id'];
+
+            APIObservationInfo.push(['UPDATE observations set `id`=? WHERE `uuid`=?', [this_obs_id, this_obs_uuid]]);
+        }
+
+        if (submitDebug) { console.log('APIObservationInfo') };
+        if (submitDebug) { console.log(APIObservationInfo) };
+
+        $rootScope.db.sqlBatch(APIObservationInfo, function () {
+            if (submitDebug) { console.log('Successfully Updated Observations with API Response') };
+            //$rootScope.db.executeSql('SELECT * FROM observations', [], function (res) {
+            //    //console.log('Sample column value: ' + res.rows.item(0));
+            //});
+        }, function (error) {
+            console.log('Unable to Update Observations with IDs from API');
+            console.log(error);
+        });
+	*/
+};
